432d38ad4492ff56860075c6ab86efaa863e050c
unknown <kulangana@yandex.ru git config --global user.name Kulangana-ddgit --global user.email kulangana@yandex.ru>
2022-12-24 22:58:13 +0300

    3 дз Division By Zero

---
+++ task3.py
@@ -0,0 +1,30 @@
+"""
+Задание 3.
+
+Создайте собственный класс-исключение, обрабатывающий ситуацию деления на нуль.
+Проверьте его работу на данных, вводимых пользователем. 
+При вводе пользователем нуля в качестве делителя 
+программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
+"""
+
+class OwnError(Exception):
+
+    def __init__(self, text):
+        self.text = text
+
+
+def division_by_zero(a, b):
+
+    try:
+        if b == 0:
+            raise OwnError("Деление на ноль невозможно")
+    except OwnError as err:
+        print(err)
+    else:
+        print(f'Частное = {int(a) / int(b)}')
+
+
+divider = int(input("Введите делимое: "))
+denominator = int(input("Введите делитель: "))
+
+division_by_zero(divider, denominator)
